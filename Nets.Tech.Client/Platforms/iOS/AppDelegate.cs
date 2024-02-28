5f51bf77b1c00ad85a50ec49c10d197c77408ccd
Tetiana Khovanska <tetiana.khovanska@softwarium.net>
2024-02-28 23:24:17 +0100

    Added Nets.Client MAUI app

---
+++ Nets.Tech.Client/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nets.Tech.Client
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
